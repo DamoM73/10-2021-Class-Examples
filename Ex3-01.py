11dd002170055f18cfa3a5b0842f45667a721b13
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-02-18 09:25:15 +1000

    Create Ex3-01.py

---
+++ Ex3-01.py
@@ -0,0 +1,9 @@
+length = float(input("Please enter length: "))
+width = float(input("Please enter width: "))
+
+area = length * width
+
+if length == width:
+    print("This is a square of area", area)
+else:
+    print("This is a rectangle of area", area)
